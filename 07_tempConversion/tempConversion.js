1c56e4f229dad468e88bd7b9d47910efe3e95bc2
vonnerbosa <vonherbosa@gmail.com>
2024-12-17 04:00:59 -0800

    getting better

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let Celsius = ((faren - 32) * (5/9))
+  return Math.round(Celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsi) {
+  let Farenheit = (celsi * (9/5)) + 32
+  return Math.round(Farenheit * 10) / 10
 };
 
 // Do not edit below this line
