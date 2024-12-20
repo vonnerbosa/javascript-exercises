613788e8202c287e42f9426fe0f296e24b5ad4d6
vonnerbosa <vonherbosa@gmail.com>
2024-12-20 05:40:10 -0800

    finished calculator test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let finalSum = 0  
+  for (let i = 0; i < nums.length; i++){
+    finalSum += nums[i];
+  }
+  return finalSum
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let finalProd = 1
+  for (let i = 0; i < nums.length; i++){
+    finalProd *= nums[i];
+  }
+  return finalProd
 };
 
-const power = function() {
-	
+const power = function(num, expo) {
+	return num ** expo
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number == 0){
+    return 1
+  } else {
+    let final = 1
+    for (let i = 1; i <= number; i++) {
+      final *= i
+    }
+    return final
+  }
 };
 
 // Do not edit below this line
